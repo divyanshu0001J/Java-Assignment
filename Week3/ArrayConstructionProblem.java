629b141153cdb1366210243ba151b6e6d6668b60
divyanshu0001J <divyanshu25csu069@ncuindia.edu>
2026-08-23 18:35:26 +0530

    Update ArrayConstructionProblem.java

--- Week3/ArrayConstructionProblem.java
+++ Week3/ArrayConstructionProblem.java
@@ -1,10 +1,10 @@
 public class ArrayConstructionProblem {
 
-    public int f_val_AfterOper(String[] operations) {
+    public int f_val_AfterOper(String[] op) {
         int x = 0;
 
         for (int i = 0; i < operations.length; i++) {
-            if (operations[i].equals("++X") || operations[i].equals("X++"))
+            if (op[i].equals("++X") || op[i].equals("X++"))
                 x++;
             else
                 x--;
@@ -13,34 +13,29 @@ public class ArrayConstructionProblem {
         return x;
     }
 
-    public int[] buildArray(int[] nums) {
-        int[] ans = new int[nums.length];
-
-        for (int i = 0; i < nums.length; i++)
-            ans[i] = nums[nums[i]];
+    public int[] buildArray(int[] arr) {
+        int[] ans = new int[arr.length];
+        for (int i = 0; i < arr.length; i++)
+            ans[i] = arr[arr[i]];
 
         return ans;
     }
 
-    public int[] getConcat(int[] nums) {
-        int[] ans = new int[nums.length * 2];
-
-        for (int i = 0; i < nums.length; i++) {
+    public int[] getConcat(int[] num) {
+        int[] ans = new int[num.length * 2];
+        for (int i = 0; i < num.length; i++) {
             ans[i] = nums[i];
-            ans[i + nums.length] = nums[i];
+            ans[i + num.length] = num[i];
         }
-
         return ans;
     }
 
-    public int[] runSum(int[] nums) {
-        int[] ans = new int[nums.length];
-
-        ans[0] = nums[0];
-
-        for (int i = 1; i < nums.length; i++)
-            ans[i] = ans[i - 1] + nums[i];
-
-        return ans;
+  public int[] runSum(int[] arr) {
+    int temp = arr[0];
+    for (int i = 1; i < arr.length; i++) {
+        arr[i] += temp;
+        temp = arr[i];
     }
+
+    return arr;
 }
